6643e561541a097d0518a08e29fbfc4d88c00184
TiaW221 <tia.willock@dvsa.gov.uk>
2025-11-05 13:47:42 +0000

    Finished Matt Work

--- src/app/Pages/page1/page1.ts
+++ src/app/Pages/page1/page1.ts
@@ -1,10 +1,10 @@
-import {Component, inject, OnInit, SimpleChanges} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {Title} from '../../Components/title/title';
 import {Button} from '../../Components/button/button';
 import {UserService} from '../../../Services/UserService/UserServices';
 import {
   CurrencyPipe,
-  DatePipe,
+  DatePipe, DecimalPipe,
   LowerCasePipe,
   PercentPipe,
   SlicePipe,
@@ -16,7 +16,7 @@ import {
 @Component({
   selector: 'app-page1',
   imports: [
-    Title, Button, UpperCasePipe, LowerCasePipe, TitleCasePipe, DatePipe, PercentPipe, CurrencyPipe, SlicePipe
+    Title, Button, UpperCasePipe, LowerCasePipe, TitleCasePipe, DatePipe, PercentPipe, CurrencyPipe, SlicePipe, DecimalPipe
   ],
   templateUrl: './page1.html',
   styleUrl: './page1.scss',
@@ -29,8 +29,21 @@ export class Page1 implements OnInit {
   private readonly userService = inject(UserService);
 
   today = new Date();
-
+// Create a variable meetingDate = new Date('2025-12-25T14:30:00')
+  meetingDate = new Date('2025-12-25T14:30:00');
 currencyValue = '1.50';
+
+// <--In your component:-->
+//   <!--price = 1234.5;-->
+//   <!--discount = 0.15;-->
+  price = 1234.5;
+  discount = 0.15;
+  finalPrice = this.price - (this.price * this.discount);
+  // <!--4. Customising Output-->
+  // <!--Create an array:-->
+  // <!--students = ['Alice', 'Bob', 'Charlie', 'Daisy', 'Ethan'];-->
+  students = ['Alice', 'Bob', 'Charlie', 'Daisy', 'Ethan']
+  quote = 'angular pipes make data formatting simple'
     ngOnInit() {
 
       console.log(this.userService.getName('Tia'));
