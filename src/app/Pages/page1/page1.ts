d3903c176d7b9b729a462e855124abec9d290308
TiaW221 <tia.willock@dvsa.gov.uk>
2025-11-05 10:13:58 +0000

    Matt Work 5th Nov

--- src/app/Pages/page1/page1.ts
+++ src/app/Pages/page1/page1.ts
@@ -1,20 +1,40 @@
-import {Component, SimpleChanges} from '@angular/core';
+import {Component, inject, OnInit, SimpleChanges} from '@angular/core';
 import {Title} from '../../Components/title/title';
 import {Button} from '../../Components/button/button';
+import {UserService} from '../../../Services/UserService/UserServices';
+import {
+  CurrencyPipe,
+  DatePipe,
+  LowerCasePipe,
+  PercentPipe,
+  SlicePipe,
+  TitleCasePipe,
+  UpperCasePipe
+} from '@angular/common';
 
 
 @Component({
   selector: 'app-page1',
   imports: [
-    Title, Button
+    Title, Button, UpperCasePipe, LowerCasePipe, TitleCasePipe, DatePipe, PercentPipe, CurrencyPipe, SlicePipe
   ],
   templateUrl: './page1.html',
   styleUrl: './page1.scss',
 })
 
-export class Page1 {
+export class Page1 implements OnInit {
   onCounterChanged(counter: number) {
     console.log(counter);
   }
-}
+  private readonly userService = inject(UserService);
+
+  today = new Date();
+
+currencyValue = '1.50';
+    ngOnInit() {
+
+      console.log(this.userService.getName('Tia'));
+    }
+  }
+
 
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
-import { Component, signal } from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {UserService} from '../Services/UserService/UserServices';
+import {DataService} from '../Services/DataService/DataService';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('rhys-angular-project');
+
+  // this is the modern way of injecting a service into another class
+  private readonly userService = inject(UserService);
+
+// This is the old way of injecting a service into another class
+constructor(
+  private readonly dataService: DataService,
+) {}
+
+  ngOnInit() {
+
+    console.log(this.userService.sayHello());
+    console.log(this.dataService.sayGoodbye());
+  }
 }
